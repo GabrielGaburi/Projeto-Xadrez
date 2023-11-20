f5f4d45207dd90512a77e92b769b256066bdb9d3
Gabriel Gaburi <gabrielgaburi@hotmail.com>
2023-11-19 21:08:31 -0300

    Primeira posição

---
+++ src/Application/Program.java
@@ -0,0 +1,16 @@
+package Application;
+
+import Tabuleiro.Posicao;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		
+	Posicao posicao = new Posicao(3,5);
+	
+	System.out.println(posicao);
+
+	}
+
+}
